705a8d34544487457096bf48cfe81eff338fb1b7
Andriana008 <andriana080999@gmail.com>
2019-03-21 21:39:08 +0200

    Update task model

--- TaskManager/Models/Task.cs
+++ TaskManager/Models/Task.cs
@@ -2,9 +2,15 @@
 namespace TaskManager.Models
 {
     public class Task
-    {
-        public Task()
-        {
-        }
+    {         public int Id { get; set; }
+         public string Name { get; set; }
+         public string Description { get; set; }
+         public TimeSpan ElapsedTime { get; set; }
+         public DateTime LastStartTime { get; set; }
+         public TimeSpan? Goal { get; set; }
+         public bool IsRunning { get; set; }
+         public int UserId { get; set; }
+         public ApplicationUser User { get; set; }
+         public WatchType WatchType { get; set; }
     }
 }
