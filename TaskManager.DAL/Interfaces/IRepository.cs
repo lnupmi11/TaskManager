1637c483d64bf24b124de261e7af336fe2172ffe
andrewDubyk <andrew.dubyk7@gmail.com>
2019-04-06 23:08:01 +0300

    Change functionality of task repository

--- TaskManager.DAL/Interfaces/IRepository.cs
+++ TaskManager.DAL/Interfaces/IRepository.cs
@@ -7,17 +7,14 @@ namespace TaskManager.DAL.Interfaces
     public interface IRepository<T> where T : class
     {
         IEnumerable<T> GetAll();
-        T Find(string id);
         IEnumerable<T> GetAllWhere(Func<T, Boolean> predicate);
         IEnumerable<T> GetAllByIds(IEnumerable<string> ids);
+        T Find(string id);
         T Find(Func<T, Boolean> predicate);
         void Create(T item);
-        void UpdateAsync(T item);
+        void Update(T item);
         void Delete(string id);
-        void RemoveAsync(T item);
+        void Delete(T item);
         bool Any(Func<T, Boolean> predicate);
-        Task CreateAsync(T item);
-        Task DeleteAsync(string id);
-        T SingleOrDefault(Func<T, Boolean> predicate);
     }
 }
