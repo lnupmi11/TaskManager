f70fa5bab009fe1c79e7d6bff1492d804c43222d
andrewDubyk <andrew.dubyk7@gmail.com>
2019-04-07 10:12:18 +0300

    Apply IUserService interface for UserService class

---
+++ TaskManager.BLL/Services/UserService.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections.Generic;
+using System.Security.Claims;
+using TaskManager.DAL.Models;
+using TaskManager.BLL.Extensions.Identity;
+using TaskManager.DAL.Interfaces;
+using TaskManager.BLL.Interfaces;
+
+namespace TaskManager.BLL.Services
+{
+    public class UserService : IUserService
+    {
+        private IRepository<UserProfile> _userRepository;
+
+        public UserService(IRepository<UserProfile> userRepository)
+        {
+            _userRepository = userRepository;
+        }
+
+        public UserProfile GetUserProfile(ClaimsPrincipal principal)
+        {
+            return _userRepository.Find(principal.GetUserId());
+        }
+
+        public UserProfile GetUserProfile(string id)
+        {
+            return _userRepository.Find(id);
+        }
+
+        public string GetUserProfileId(ClaimsPrincipal principal)
+        {
+            return principal.GetUserId();
+        }
+
+        public IEnumerable<UserProfile> GetUserProfiles()
+        {
+            return _userRepository.GetAll();
+        }
+
+        public void ChangeFirstName(UserProfile user, string firstName)
+        {
+            user.FirstName = firstName;
+            _userRepository.Update(user);
+        }
+
+        public void ChangeFirstName(ClaimsPrincipal principal, string firstName)
+        {
+            var user = GetUserProfile(principal);
+            ChangeFirstName(user, firstName);
+        }
+
+        public void ChangeSecondName(UserProfile user, string lastName)
+        {
+            user.LastName = lastName;
+            _userRepository.Update(user);
+        }
+
+        public void ChangeSecondName(ClaimsPrincipal principal, string secondName)
+        {
+            var user = GetUserProfile(principal);
+            ChangeSecondName(user, secondName);
+        }
+
+        public void Update(UserProfile user)
+        {
+            _userRepository.Update(user);
+        }
+
+        public void Delete(UserProfile user)
+        {
+            _userRepository.Delete(user);
+        }
+    }
+}
