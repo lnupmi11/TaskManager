f970c2c760606fd2a0047ea2b46f8b675253982e
Andriana008 <andriana080999@gmail.com>
2019-04-22 22:32:29 +0300

    Update test

---
+++ TaskManager.BLL/Extensions/Identity/IIdentityExtension.cs
@@ -0,0 +1,11 @@
+﻿using System.Security.Claims;
+
+namespace TaskManager.BLL.Extensions.Identity
+{
+    public interface IIdentityExtension
+    {
+        string GetUserId(ClaimsPrincipal user);
+        string GetUserName(ClaimsPrincipal user);
+        string GetUserRole(ClaimsPrincipal user);
+    }
+}
