27c53a66754881cf0470b5aba946dc8a3059c116
andrewDubyk <andrew.dubyk7@gmail.com>
2019-05-11 15:02:46 +0300

    Implement functionality to reset password

---
+++ TaskManager/Extensions/Email/EmailSettings.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace TaskManager.Extensions.Email
+{
+    public class EmailSettings
+    {
+        public String PrimaryDomain { get; set; }
+
+        public int PrimaryPort { get; set; }
+
+        public String SecondayDomain { get; set; }
+
+        public int SecondaryPort { get; set; }
+
+        public String UsernameEmail { get; set; }
+
+        public String UsernamePassword { get; set; }
+
+        public String FromEmail { get; set; }
+
+        public String ToEmail { get; set; }
+    }
+}
--- TaskManager/Extensions/Url/UrlHelperExtensions.cs
+++ TaskManager/Extensions/Url/UrlHelperExtensions.cs
@@ -4,5 +4,13 @@ namespace Microsoft.AspNetCore.Mvc
 {
     public static class UrlHelperExtensions
     {
+        public static string ResetPasswordCallbackLink(this IUrlHelper urlHelper, string userId, string code, string scheme)
+        {
+            return urlHelper.Action(
+                action: nameof(AccountController.ResetPassword),
+                controller: "Account",
+                values: new { userId, code },
+                protocol: scheme);
+        }
     }
 }
